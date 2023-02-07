8ec355de060960d808cc4a81a0ec3b26e59fb71b
santtuniskanen <santtuniska02@gmail.com>
2023-02-07 09:53:09 +0200

    node examples

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port='3000';
+
+app.get('/', function(request, response){
+    console.log("test1");
+    response.send("Express example");
+});
+app.get('/esim1', function(request, response){
+    response.send("This is endpoint /esim1");
+});
+
+app.listen(port, function(){
+    console.log("Application listens to port " + port);
+});
