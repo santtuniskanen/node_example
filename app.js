290c2c18ee8cf4acaf7aaad4c443c69c6093f002
santtuniskanen <santtuniska02@gmail.com>
2023-02-20 16:19:19 +0200

    changes....

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ app.get('/', function(request, response){
     console.log("test1");
     response.send("Express Example");
 });
-app.get('/esim1', function(request, response){
+app.get('/example1', function(request, response){
     response.send("This is endpoint /esim1...");
 });
 
 app.get('/example2/:fname?', function(request, response){
-    response.send("Hello "+request.params.fname)
+    response.send("Hello, "+request.params.fname)
+});
+
+app.post('/', function(request, response){
+    response.send("Post method was called");
+    console.log("Post method was called");
 });
 
 app.listen(port, function(){
