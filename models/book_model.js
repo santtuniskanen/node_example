0251fe9b330a3cac4086a3ea1031c7e1507b9a10
santtuniskanen <santtuniska02@gmail.com>
2023-02-20 18:34:48 +0200

    Emulated database

---
+++ models/book_model.js
@@ -0,0 +1,5 @@
+const bookArray=[
+    {name:"C++", author:"Teppo Testi", isbn:"12-34-56-77"},
+    {name:"JavaScript", author:"Aino Testi", isbn:"12-34-56-77"},
+    {name:"Databases", author:"Daniel Datafield", isbn:"12-34-56-77"}
+]
