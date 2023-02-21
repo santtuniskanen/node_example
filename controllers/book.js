6f4b0127902b14d0cd8528a23b31e294b65c63f8
santtuniskanen <santtuniska02@gmail.com>
2023-02-21 16:46:33 +0200

    Things done

--- controllers/book.js
+++ controllers/book.js
@@ -1,27 +1,33 @@
 const express=require('express'); // Refers to express installed to node modules
 const controller=express.Router();
+const book=require('../models/book_model');
 
 // Implementing all book-tables CRUD-operations
 // Create Read Update Delete
 controller.get('/', function(request, response){
-    response.json("Returns all books");
+    let data=book.getAllBooks();
+    response.json(data);
 });
 
 controller.get('/:id', function(request, response){
-    response.json("Returns a book, where id=" + request.params.id);
+    let data=book.getOneBook(request.params.id);
+    response.json(data);
 })
 
 controller.post('/', function(request, response){
-    response.json(request.body);
+    let data=book.addBook(request.body);
+    response.json(data);
 })
 
 controller.put('/:id', function(request, response){
-    response.json("Changes the book where id="+request.params.id);
+    let data=book.updateBook(request.params.id, request.body);
+    response.json(data);
     console.log(request.body);
 })
 
 controller.delete('/:id', function(request, response){
-    response.json("Deletes the book where id="+request.params.id);
+    let data=book.deleteBook(request.params.id);
+    response.json(data);
     console.log("Deletes the book where id="+request.params.id);
 })
 
