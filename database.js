261cb76344bef168356afb88ec27f86f49229991
santtuniskanen <santtuniska02@gmail.com>
2023-02-22 19:39:01 +0200

    database.js changes

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql');
 
-//const connectionString="mysql://tvt22spo:tvtpass@localhost:3306/library";
-// const connection = mysql.createPool(connectionString);
 const connection = mysql.createPool(process.env.mysqlString);
+
 module.exports=connection;
